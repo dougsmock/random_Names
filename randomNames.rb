8314b30fd7c24a261f25a9778c3013660901d616
Your Name <you@example.com>
2018-07-11 09:06:31 -0400

    Test commit 2.

---
+++ randomNames.rb
@@ -0,0 +1,7 @@
+# Random Pair Function: Make a function 
+# that takes in array of names(as strings) -
+# Returns an array of arrays of random names(as strings)
+# - two names per array unless there is one left over
+# then pass this into existing array.
+# Please make a new repo for this
+# and do this in a tdd way.
---
+++ test_random.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require_relative "randomNames.rb"
+
+class RandomNames < Minitest::Test
+
+  def test_addition_answer
+    assert_equal(1, 1)
+  end
+
+end
