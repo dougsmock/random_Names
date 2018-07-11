87bd3d1acaa0247be8c3731180cf410750ead37f
Your Name <you@example.com>
2018-07-11 10:09:50 -0400

    test in declaring names in array

--- randomNames.rb
+++ randomNames.rb
@@ -1,7 +1,18 @@
-# Random Pair Function: Make a function 
+# Random Pair Function: Make a function
 # that takes in array of names(as strings) -
 # Returns an array of arrays of random names(as strings)
 # - two names per array unless there is one left over
 # then pass this into existing array.
 # Please make a new repo for this
 # and do this in a tdd way.
+
+
+def arr()
+
+  people = ["Tom", "Dick", "Harry", "Jenny", "Sally"]
+
+
+
+
+
+end
--- test_random.rb
+++ test_random.rb
@@ -3,8 +3,26 @@ require_relative "randomNames.rb"
 
 class RandomNames < Minitest::Test
 
-  def test_addition_answer
-    assert_equal(1, 1)
+  def test_if_arr_includes_Tom
+    assert_equal(true, arr.include?("Tom"))
   end
+
+  def test_if_arr_includes_Dick
+    assert_equal(true, arr.include?("Dick"))
+  end
+
+  def test_if_arr_includes_Harry
+    assert_equal(true, arr.include?("Harry"))
+  end
+
+  def test_if_arr_includes_Jenny
+    assert_equal(true, arr.include?("Jenny"))
+  end
+
+  def test_if_arr_includes_Sally
+    assert_equal(true, arr.include?("Sally"))
+  end
+
+
 
 end
